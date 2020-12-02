938b66a2d533869c634d9531d20e56075a9b748b
imarinho-dev <imarinho.dev@gmail.com>
2020-12-02 06:54:54 -0300

    implement code that pulls in our shop data from firestore and stores it the shop reducer from our shop page. Also included our withspinner HOC

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 
 import FormInput from '../form-input/form-input.component';
-
 import CustomButton from '../custom-button/custom-button.component';
 
 import { auth, signInWithGoogle } from '../../firebase/firebase.utils';
 
-import './sign-in.styles.scss';
+import {
+	SignInContainer,
+	SignInTitle,
+	ButtonsBarContainer,
+} from './sign-in.styles';
 
 class SignIn extends React.Component {
 	constructor(props) {
@@ -39,9 +42,10 @@ class SignIn extends React.Component {
 
 	render() {
 		return (
-			<div className='sign-in'>
-				<h2 className='title'>I already have an account</h2>
+			<SignInContainer>
+				<SignInTitle>I already have an account</SignInTitle>
 				<span>Sign in with your email and password</span>
+
 				<form onSubmit={this.handleSubmit}>
 					<FormInput
 						name='email'
@@ -54,23 +58,19 @@ class SignIn extends React.Component {
 					<FormInput
 						name='password'
 						type='password'
-						handleChange={this.handleChange}
 						value={this.state.password}
+						handleChange={this.handleChange}
 						label='password'
 						required
 					/>
-					<div className='buttons'>
-						<CustomButton type='submit'>SIGN IN </CustomButton>
-						<CustomButton
-							type='button'
-							onClick={signInWithGoogle}
-							isGoogleSignIn
-						>
-							Sign in with Google{' '}
+					<ButtonsBarContainer>
+						<CustomButton type='submit'> Sign in </CustomButton>
+						<CustomButton onClick={signInWithGoogle} isGoogleSignIn>
+							Sign in with Google
 						</CustomButton>
-					</div>
+					</ButtonsBarContainer>
 				</form>
-			</div>
+			</SignInContainer>
 		);
 	}
 }
